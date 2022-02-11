99ea9d1028a6cb4caabf8014b313b790b70f8013
Solovman <isolomen@mail.ru>
2022-02-11 14:38:29 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -3,7 +3,8 @@
 $very_bad_unclear_name="15 chicken wings";
 
 // Write your code here:
-
+$order = &$very_bad_unclear_name;
+$order .=" and Pepsi";
 
 //// Don't change the line below
 echo"\nYour order is:$very_bad_unclear_name.";
